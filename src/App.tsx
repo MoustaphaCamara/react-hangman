d1f6fe9fd4b9b7340ed9082750293d52449d9822
Mouss <104678778+MoussCarter@users.noreply.github.com>
2022-11-28 23:59:44 +0100

    v1

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import words from "./wordList.json";
+import HangmanDrawing from "./components/HangmanDrawing";
+import HangmanWord from "./components/HangmanWord";
+import Keyboard from "./components/Keyboard";
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return "test";
+    // return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  const incorrectLetters = guessedLetters.filter(
+    (letter) => !wordToGuess.includes(letter)
+  );
+  console.log(wordToGuess);
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        Lose Win
+      </div>
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HangmanWord.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+const word = "test";
+const guessedLetters = ["e", "s"];
+const HangmanWord = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "JetBrains Mono",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default HangmanWord;
