3b019724fbfd6442a37ed88591b8ee0b14ed5962
Moustapha Camara <cmr.mous@gmail.com>
2022-11-29 10:22:24 +0100

    refactoring stylesheet hangman drawing

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,86 +1,9 @@
-import React from "react";
-
-const HEAD = (
-  <div
-    style={{
-      width: "50px",
-      height: "50px",
-      borderRadius: "100%",
-      border: "10px solid black",
-      position: "absolute",
-      top: "50px",
-      right: "-30px",
-    }}
-  ></div>
-);
-const BODY = (
-  <div
-    style={{
-      width: "10px",
-      height: "100px",
-      background: "black",
-      position: "absolute",
-      top: "120px",
-      right: 0,
-    }}
-  ></div>
-);
-const RIGHT_ARM = (
-  <div
-    style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "150px",
-      right: "-100px",
-      rotate: "-30deg",
-      transformOrigin: "left bottom",
-    }}
-  ></div>
-);
-const LEFT_ARM = (
-  <div
-    style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "150px",
-      right: "10px",
-      rotate: "30deg",
-      transformOrigin: "right bottom",
-    }}
-  ></div>
-);
-const RIGHT_LEG = (
-  <div
-    style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "210px",
-      right: "-90px",
-      rotate: "60deg",
-      transformOrigin: "left bottom",
-    }}
-  ></div>
-);
-const LEFT_LEG = (
-  <div
-    style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "210px",
-      right: "0",
-      rotate: "-60deg",
-      transformOrigin: "right bottom",
-    }}
-  ></div>
-);
+const HEAD = <div className="head"></div>;
+const BODY = <div className="body"></div>;
+const RIGHT_ARM = <div className="right-arm"></div>;
+const LEFT_ARM = <div className="left-arm"></div>;
+const RIGHT_LEG = <div className="right-leg"></div>;
+const LEFT_LEG = <div className="left-leg"></div>;
 
 const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
 type HangmanDrawingProps = {
